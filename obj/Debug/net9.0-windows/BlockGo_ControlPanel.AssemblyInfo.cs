d70c42676c2f4ff4a72228517a94700df915c597
Jeremy KuLee <Jeremy6234709573@gmail.com>
2025-09-19 01:17:38 +0800

    0919

--- obj/Debug/net9.0-windows/BlockGo_ControlPanel.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/BlockGo_ControlPanel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockGo_ControlPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb56786d583184df3e827bacc5cdbc7f0cc8bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockGo_ControlPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockGo_ControlPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
